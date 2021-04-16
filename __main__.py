44d89de517925c818dc145683c522389c178728d
Kaiwen Xu <kaiwen.dev@gmail.com>
2021-04-16 10:16:00 -0500

    Generate lung mask for spore

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ from lungmask import mask
 from lungmask import utils
 import os
 import SimpleITK as sitk
-from lungmask.utils import read_file_contents_list
+from lungmask.utils import read_file_contents_list, save_file_contents_list
 
 
 def path(string):
@@ -32,6 +32,7 @@ def main():
                         default=out_mask_folder)
     parser.add_argument('--file-list-txt', type=str,
                         default=file_list_txt)
+    parser.add_argument('--failed-list-txt', type=str)
     parser.add_argument('--modeltype', help='Default: unet', type=str, choices=['unet'], default='unet')
     parser.add_argument('--modelname', help="spcifies the trained model, Default: R231", type=str, choices=['R231','LTRCLobes','LTRCLobes_R231','R231CovidWeb'], default='R231')
     parser.add_argument('--cpu', help="Force using the CPU even when a GPU is available, will override batchsize to 1", action='store_true')
@@ -49,27 +50,35 @@ def main():
     logging.info(f'Load model')
 
     file_list = read_file_contents_list(args.file_list_txt)
+    failed_case_list = []
     for file_idx in range(len(file_list)):
         file_name = file_list[file_idx]
         logging.info(f'Process {file_name}, {file_idx} / {len(file_list)}')
-        in_nii = os.path.join(args.in_folder, file_name)
-        out_nii_mask = os.path.join(args.out_folder, file_name)
 
-        input_image = utils.get_input_image(in_nii)
-        logging.info(f'Infer lungmask')
-        if args.modelname == 'LTRCLobes_R231':
-            result = mask.apply_fused(input_image, force_cpu=args.cpu, batch_size=batchsize,
-                                      volume_postprocessing=not (args.nopostprocess), noHU=args.noHU)
-        else:
-            model = mask.get_model(args.modeltype, args.modelname)
-            result = mask.apply(input_image, model, force_cpu=args.cpu, batch_size=batchsize,
-                                volume_postprocessing=not (args.nopostprocess), noHU=args.noHU)
+        try:
+            in_nii = os.path.join(args.in_folder, file_name)
+            out_nii_mask = os.path.join(args.out_folder, file_name)
 
-        result_out = sitk.GetImageFromArray(result)
-        result_out.CopyInformation(input_image)
-        logging.info(f'Save result to: {out_nii_mask}')
-        # sys.exit(sitk.WriteImage(result_out, out_nii_mask))
-        sitk.WriteImage(result_out, out_nii_mask)
+            input_image = utils.get_input_image(in_nii)
+            logging.info(f'Infer lungmask')
+            if args.modelname == 'LTRCLobes_R231':
+                result = mask.apply_fused(input_image, force_cpu=args.cpu, batch_size=batchsize,
+                                          volume_postprocessing=not (args.nopostprocess), noHU=args.noHU)
+            else:
+                model = mask.get_model(args.modeltype, args.modelname)
+                result = mask.apply(input_image, model, force_cpu=args.cpu, batch_size=batchsize,
+                                    volume_postprocessing=not (args.nopostprocess), noHU=args.noHU)
+
+            result_out = sitk.GetImageFromArray(result)
+            result_out.CopyInformation(input_image)
+            logging.info(f'Save result to: {out_nii_mask}')
+            # sys.exit(sitk.WriteImage(result_out, out_nii_mask))
+            sitk.WriteImage(result_out, out_nii_mask)
+        except:
+            print(f'Something wrong with {file_name}')
+            failed_case_list.append(file_name)
+
+    save_file_contents_list(args.failed_list_txt, failed_case_list)
 
 
 if __name__ == "__main__":
